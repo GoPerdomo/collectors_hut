58db7d8d537d6cb0bbdaf0bba8ce2fa30c660953
Gabriel de O. Perdomo <goperdomo@gmail.com>
2017-11-28 18:08:08 +0100

    Set profile structure

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -2,16 +2,36 @@ import React, { Component } from 'react';
 
 import './style.css';
 
-class ProfileContainer extends Component {
+class Profile extends Component {
   
   render() {
     return (
       <div className="tempWrapper">
         <header className="Header">
-          <p>Collectors Hut Hearder</p>
+          <p>Collectors Hut Header</p>
         </header>
         <main className="Profile">
-
+          <section className="profile-header">
+          <section className="profile-cover"></section>
+          <section className="profile-info">
+            <img src="https://cdn3.iconfinder.com/data/icons/black-easy/512/535106-user_512x512.png" alt="TODO: Add a nonredundant alt"/>
+            <p>John Userman</p>
+          </section>
+          </section>
+          <section className="collections">
+            <section className="collection">
+              <div className="collection-photos-preview"></div>
+              <div className="collection-description"></div>
+            </section>
+            <section className="collection">
+              <div className="collection-photos-preview"></div>
+              <div className="collection-description"></div>
+            </section>
+            <section className="collection">
+              <div className="collection-photos-preview"></div>
+              <div className="collection-description"></div>
+            </section>
+          </section>
         </main>
         <footer className="Footer">
           <p>Collectors Hut Footer</p>
@@ -21,4 +41,4 @@ class ProfileContainer extends Component {
   }
 };
 
-export default ProfileContainer;
+export default Profile;
