056791753de5466ebb29da1a09c21390d0bd079d
Gabriel de O. Perdomo <goperdomo@gmail.com>
2017-12-01 01:35:38 +0100

    Small changes on style

--- src/containers/ItemInfo/index.js
+++ src/containers/ItemInfo/index.js
@@ -10,11 +10,11 @@ class ItemInfo extends Component {
     return (
       <div className="item-display">
       <div className="item-photo">
-        <img src={ currentItem.photo }/>
+        <img src={ currentItem.photo } alt="TODO: Add nonredundant alt"/>
       </div>
       <div className="item-info">
         <h2>{ currentItem.name }</h2>
-        <ul>
+        <ul className="item-info-list">
           <li>Description: { currentItem.description }</li>
           <li>Production Year: { currentItem.productionYear }</li>
           <li>Acquisition Year: { currentItem.acquisitionYear }</li>
@@ -23,7 +23,6 @@ class ItemInfo extends Component {
           <li>Condition: { currentItem.condition }</li>
         </ul>
       </div>
-
       </div>
     )
   }
