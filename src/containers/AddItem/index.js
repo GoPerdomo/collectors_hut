650196e9f9e8190e62523777ccea6683f043175e
Gabriel de O. Perdomo <goperdomo@gmail.com>
2017-12-06 00:08:43 +0100

    Implement and style AddItem form

--- src/containers/AddItem/index.js
+++ src/containers/AddItem/index.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 import ConfigButton from '../../containers/ConfigButton';
+
+
+// TODO: Refactor
 
 class AddItem extends Component {
 
@@ -12,41 +17,122 @@ class AddItem extends Component {
 
     this.state = {
       open: false,
+      newItem: {
+        name: "",
+        description: "",
+        productionYear: "",
+        acquisitionYear: "",
+        origin: "",
+        manufacturer: "",
+        condition: "",
+      },
     };
   }
 
-  handleButtonClick = (event) => {
-    this.setState({
-      open: true,
-      anchorEl: event.currentTarget,
-    })
+  handleSubmit = (event) => {
+    event.preventDefault();    
   }
 
-  handleRequestClose = () => {
+  handleContentChange = (event, content) => {
+    let {
+      name,
+      description,
+      productionYear,
+      acquisitionYear,
+      origin,
+      manufacturer,
+    } = this.state.newItem;
+    const { id } = event.target;
+
+    if (id === "new-item-name") name = content;
+    if (id === "new-item-description") description = content;
+    if (id === "new-item-productionYear") productionYear = content;
+    if (id === "new-item-acquisitionYear") acquisitionYear = content;
+    if (id === "new-item-origin") origin = content;
+    if (id === "new-item-manufacturer") manufacturer = content;
+
     this.setState({
-      open: false,
+      newItem: {
+        name,
+        description,
+        productionYear,
+        acquisitionYear,
+        origin,
+        manufacturer,
+      }
     });
   }
 
   render() {
+    const {
+      name,
+      description,
+      productionYear,
+      acquisitionYear,
+      origin,
+      manufacturer
+    } = this.state.newItem;
+
     return (
       <ConfigButton label="Add item">
         <form onSubmit={this.handleSubmit}>
           <TextField
-            // id="new-collection-name"
-            // hintText="Name"
+            id="new-item-name"
+            hintText="Name"
+            errorText={!name && "Name is required"}
             fullWidth
             onChange={this.handleContentChange}
-          // value={this.state.newCollection.name}
-          /><br />
+            value={name}
+          />
           <TextField
-            // id="new-collection-info"
-            // hintText="Description"
+            id="new-item-description"
+            hintText="Description"
             fullWidth
             multiLine
             onChange={this.handleContentChange}
-          // value={this.state.newCollection.info}
-          /><br />
+            value={description}
+          />
+          <TextField
+            id="new-item-productionYear"
+            hintText="Production Year"
+            type="number"
+            fullWidth
+            onChange={this.handleContentChange}
+            value={productionYear}
+          />
+          <TextField
+            id="new-item-acquisitionYear"
+            hintText="Acquisition Year"
+            type="number"
+            fullWidth
+            onChange={this.handleContentChange}
+            value={acquisitionYear}
+          />
+          <TextField
+            id="new-item-origin"
+            hintText="Origin"
+            fullWidth
+            onChange={this.handleContentChange}
+            value={origin}
+          />
+          <TextField
+            id="new-item-manufacturer"
+            hintText="Manufacturer"
+            fullWidth
+            onChange={this.handleContentChange}
+            value={manufacturer}
+          />
+          <SelectField
+            id="new-item-condition"
+            onChange={(event, value) => this.setState({ conditionValue: value })}
+            value={this.state.conditionValue}
+            hintText="Condition"
+          >
+            <MenuItem value={0} primaryText={"Mint"} />
+            <MenuItem value={1} primaryText={"Good"} />
+            <MenuItem value={2} primaryText={"Fair"} />
+            <MenuItem value={3} primaryText={"Poor"} />
+          </SelectField>
           <RaisedButton type="submit" label="Create" fullWidth />
         </form>
       </ConfigButton>
