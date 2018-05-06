4f56f1b3cfa88337dcf37799f3cbe10590761f77
Gabriel de O. Perdomo <goperdomo@gmail.com>
2018-05-06 20:16:56 +0200

    Fix max-width

--- src/routes/Search/index.js
+++ src/routes/Search/index.js
@@ -14,7 +14,7 @@ import bp from '../../helpers/breakpoints';
 // ========== Styled Components ==========
 const SearchWrapper = styled.main`
   width: 65%;
-  max-width: ${bp.maxWidth};
+  max-width: ${bp.breakTwo};
   margin: 0 auto auto;
   text-align: center;
 
